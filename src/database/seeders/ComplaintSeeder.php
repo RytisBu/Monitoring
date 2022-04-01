7aaa798ba2ca8fe3cd0d1c038ba2b1ac54073cdc
Rytis Vasiliauskas <Rytis.Vasiliauskas@bite.lt>
2022-04-01 17:33:02 +0300

    Added migration and core stuff of complaints

---
+++ src/database/seeders/ComplaintSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Complaint;
+use Illuminate\Database\Seeder;
+
+class ComplaintSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Complaint::factory()->times(1)->create();
+    }
+}
